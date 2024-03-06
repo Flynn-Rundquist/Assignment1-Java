f4a0f08af2a1a0e2a983b33858ede20f0d5b6350
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-03-06 14:04:58 -0500

    Update ScissorsPaperRock.java

--- ScissorsPaperRock.java
+++ ScissorsPaperRock.java
@@ -8,37 +8,40 @@
  */
 
 import java.util.Scanner;
-import java.util,Random;
+import java.util.Random;
 
 public class ScissorsPaperRock {
-  public static void main(String[] args) {
-    //Array w/ options of rock, paper, or scissors
-    String[] options = {"Rock", "Paper", "Scissors"};
+    public static void main(String[] args) {
+        // Array w/ options of rock, paper, or scissors
+        String[] options = {"Rock", "Paper", "Scissors"};
 
-    // Gets user input
-    Scanner myObj = new Scanner(System.in); //Creates scanner object
-    System.out.println("Choose rock (1), paper (2), or scissors(3): ")
-    int userChoice = scanner.nextInt() - 1; //Subtract 1 because arrays start at 0
-    System.out.println("You selected: " +options[userChoice]);
+        // Gets user input
+        Scanner scanner = new Scanner(System.in); // Creates scanner object
+        System.out.println("Choose rock (1), paper (2), or scissors (3): ");
+        int userChoice = scanner.nextInt() - 1; // Subtract 1 because arrays start at 0
+        System.out.println("You selected: " + options[userChoice]);
 
-    // Gets computers selection
-    Random random = new Random();
-    int computerChoice = random.nextInt(options.length);
+        // Gets computer's selection
+        Random random = new Random();
+        int computerChoice = random.nextInt(options.length);
 
-    // print computers choiec
-    System.out.println("Computer selected: " +options[computerChoice]);
+        // Print computer's choiec
+        System.out.println("Computer selected: " + options[computerChoice]);
 
-    if (userChoice < 3 && userChoice > 0 ) {
-        if (userChoice === computerChoice) {
+        if (userChoice >= 0 && userChoice < options.length) {
+            if (userChoice == computerChoice) {
                 System.out.println("Draw!");
-        } else if (
-                        (userChoiceIndex == 0 && computerChoiceIndex == 2) ||
-            (userChoiceIndex == 1 && computerChoiceIndex == 0) ||
-            (userChoiceIndex == 2 && computerChoiceIndex == 1)
-        ) {
-            System.out.println("You win!");
-        } else {
+            } else if (
+                    (userChoice == 0 && computerChoice == 2) ||
+                    (userChoice == 1 && computerChoice == 0) ||
+                    (userChoice == 2 && computerChoice == 1)
+            ) {
+                System.out.println("You win!");
+            } else {
                 System.out.println("You lose!");
+            }
+        } else {
+            System.out.println("Invalid input. Input a number between 1 and 3.");
         }
-    } else {
-        System.out.println("Invalid input. Input a number between 1 and 3.")
+    }
+}
