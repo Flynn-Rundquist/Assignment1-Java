e2814c9842af1a29adb9007072c7bb4c40727d36
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-03-06 14:40:12 +0000

    Started java part of assignemnt 1

---
+++ RockPaperScissors.java
@@ -0,0 +1,14 @@
+/*
+ * This program plays rock, paper, scissors,
+ * against the user
+ *
+ * @author Flynn Rundquist
+ * @version 1.0
+ * @since 2020-09-01
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class RockPaperScissors {
+
